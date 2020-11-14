2fd50ec66ad10f891b432525cb7983713d9fcedb
Tomek <tmszjaros@gmail.com>
2020-11-14 11:06:36 +0100

    feat(app): AO

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './component/home/home.component';
+import {PostsListComponent} from './component/posts-list/posts-list.component';
+import {AboutComponent} from './component/about/about.component';
+import {PageNotFoundComponent} from './component/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'posts',
+    component: PostsListComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { AboutComponent } from './component/about/about.component';
+import { PostItemComponent } from './component/post-item/post-item.component';
+import { PostsListComponent } from './component/posts-list/posts-list.component';
+import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    PostItemComponent,
+    PostsListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
